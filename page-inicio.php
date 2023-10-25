0bb38d8184bd6e18f64804ba76cecd01f6a22d7b
Pato Marques <patoxmarques@gmail.com>
2023-10-25 00:21:53 -0300

    starting theme development

---
+++ page-inicio.php
@@ -0,0 +1,65 @@
+<?php get_header(); ?>
+
+<section id="section-" class="container-full">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="bg-fullscreen">
+                    <h2 class="bg-fullscreen__title title-section bold">Pato Marques</h2>
+                    <h3 class="bg-fullscreen__subtitle">Wordpress, Laravel, PHP, Front-end, Back-end.</h3>
+                </div>                
+            </div>
+        </div>
+    </div>
+</section>
+<section id="section-about" class="container-full">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="content-about">
+                    <h3 class="content-about__title">Sobre</h3>
+                    <h4 class="content-about__subtitle">Desenvolvedor Web desde 2010.</h4>
+                    <h5 class="content-about__description">Recife, 34 anos, Bike Fixa, Veganismo Popular, Anti-fascismo, Anti-Racismo, LGBTQIA+, Direitos Humanos e Animais.</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="section-" class="container-full">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="title-section bold"></h3>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="section-" class="container-full">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="title-section bold"></h3>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="section-" class="container-full">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="title-section bold"></h3>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="section-" class="container-full">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="title-section bold"></h3>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
