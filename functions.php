b914ba22f44593fdddfbce4f07f1e39274f33f65
Pato Marques <patoxmarques@gmail.com>
2023-10-27 00:19:51 -0300

    homepage development, custom post types

--- functions.php
+++ functions.php
@@ -2,7 +2,8 @@
 
 // style and scripts
 add_action('wp_enqueue_scripts', 'bootscore_child_enqueue_styles');
-function bootscore_child_enqueue_styles() {
+function bootscore_child_enqueue_styles()
+{
 
   // style.css
   wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
@@ -15,3 +16,46 @@ function bootscore_child_enqueue_styles() {
   // custom.js
   wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/js/custom.js', false, '', true);
 }
+
+function wporg_custom_post_type()
+{
+  register_post_type(
+    'experiences',
+    array(
+      'labels' => array(
+        'name' => __('Experiências', 'textdomain'),
+        'singular_name' => __('Experiência', 'textdomain'),
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'supports' => array('title', 'editor', 'custom-fields')
+    )
+  );
+
+  register_post_type(
+    'portfolio',
+    array(
+      'labels' => array(
+        'name' => __('Portfolio', 'textdomain'),
+        'singular_name' => __('Portfolio', 'textdomain'),
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'supports' => array('title', 'editor', 'custom-fields', 'thumbnail')
+    )
+  );
+
+  register_post_type(
+    'contatos',
+    array(
+      'labels' => array(
+        'name' => __('Contatos', 'textdomain'),
+        'singular_name' => __('Contato', 'textdomain'),
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'supports' => array('title', 'editor', 'custom-fields', 'thumbnail')
+    )
+  );
+}
+add_action('init', 'wporg_custom_post_type');
--- page-inicio.php
+++ page-inicio.php
@@ -1,62 +1,222 @@
 <?php get_header(); ?>
 
-<section id="section-" class="container-full">
+<section id="section-banner" class="container-full">
     <div class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 text-center">
                 <div class="bg-fullscreen">
                     <h2 class="bg-fullscreen__title title-section bold">Pato Marques</h2>
-                    <h3 class="bg-fullscreen__subtitle">Wordpress, Laravel, PHP, Front-end, Back-end.</h3>
-                </div>                
+                    <h3 class="bg-fullscreen__subtitle">Desenvolvedor WEB | Wordpress | Laravel | Front-end | Back-end
+                    </h3>
+                </div>
+                <a href="" class="btn-scroll-down">
+                    <i class="fa-solid fa-angles-down"></i>
+                </a>
             </div>
         </div>
     </div>
 </section>
-<section id="section-about" class="container-full">
+
+<section id="section-about" class="container-full content-main">
     <div class="container">
         <div class="row">
             <div class="col-12">
                 <div class="content-about">
-                    <h3 class="content-about__title">Sobre</h3>
-                    <h4 class="content-about__subtitle">Desenvolvedor Web desde 2010.</h4>
-                    <h5 class="content-about__description">Recife, 34 anos, Bike Fixa, Veganismo Popular, Anti-fascismo, Anti-Racismo, LGBTQIA+, Direitos Humanos e Animais.</h5>
+                    <h3 class="content-about__title title-section">Sobre</h3>
+                    <h4 class="content-about__subtitle">Desde 2010 atuando no desenvolvimento de
+                        sistemas web, sites e lojas online.</h4>
+                    <h5 class="content-about__description">Recife, 34 anos, Sistemas de Informação, Desenvolvedor Web.
+                    </h5>
+                    <h5 class="content-about__text">Bike, Veganismo Popular, Anti-fascismo,
+                        Anti-Racismo, LGBTQIA+, Direitos Humanos e Animais.</h5>
+                    </h5>
+                    <p class="hide">Planejamento, modelagem banco de dados, criação, prototipação, desenvolvimento e
+                        testes.</p>
                 </div>
             </div>
         </div>
     </div>
 </section>
-<section id="section-" class="container-full">
+
+<section id="section-skills" class="container-full content-main">
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h3 class="title-section bold"></h3>
+                <div class="content-skills">
+                    <h3 class="content-skills__title title-section bold">Tecnologias</h3>
+                    <h4 class="content-skills_description">Essas foram as tecnologias que trabalhei nos ultimos anos.
+                    </h4>
+
+                    <?php
+                    $tecnologies = ['html5', 'css3', 'javascript', 'sql', 'git', 'vue', 'react', 'nodeJS', 'responsive design', 'php', 'wordpress', 'laravel'];
+                    ?>
+
+                    <ul class="content-skills_list list-inline">
+
+                        <?php for ($i = 0; $i < count($tecnologies); $i++) { ?>
+
+                            <li class="content-skills_list_item list-inline-item">
+                                <div class="content-skills progress-bar"></div>
+                                <?= $tecnologies[$i] ?>
+                            </li>
+
+                        <?php } ?>
+
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
 </section>
-<section id="section-" class="container-full">
+
+<section id="section-experience" class="container-full content-main">
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h3 class="title-section bold"></h3>
+                <h3 class="title-section">Experiência</h3>
+                <div class="content-experience">
+                    Timeline.js com resumo dos anos, desde 2010 até 2023, com os principais trampos que passei.
+                </div>
             </div>
         </div>
     </div>
 </section>
-<section id="section-" class="container-full">
+
+<section id="section-portfolio" class="container-full content-main">
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h3 class="title-section bold"></h3>
+                <h3 class="title-section bold">Portfolio</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-4">
+                Lula Cortes
+            </div>
+            <div class="col-12 col-md-4">
+                Eve Queiroz
+            </div>
+            <div class="col-12 col-md-4">
+                Bia Ritzzz
             </div>
         </div>
     </div>
+
 </section>
-<section id="section-" class="container-full">
+
+<?php
+$args = array(
+    'numberposts' => 4,
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'post_type' => 'post',
+    'suppress_filters' => true,
+);
+
+$get_posts = new WP_Query();
+$posts = get_posts($args);
+?>
+
+<section id="section-blog" class="container-full content-main">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h3 class="title-section bold">Blog</h3>
+            </div>
+        </div>
+    </div>
+    <div class="content-blog">
+
+        <?php foreach ($posts as $post) { ?>
+            <div class="content-blog__post">
+                <div class="content-blog__post__image box-square">
+                    <?php
+                        if (!empty(get_the_post_thumbnail($post->ID, 'medium'))) {
+                            echo get_the_post_thumbnail($post->ID, 'medium');
+                        } else {
+                            echo '<i class="fa-solid fa-images"></i>';
+                        }
+                    ?>
+
+                    <div class="content-blog__post__categories">
+
+                        <ul class="content-blog__post__categories-list list-inline">
+
+                            <?php
+                            $categories = get_categories($post->ID);
+                            foreach ($categories as $category) { ?>
+
+                                <li class="content-blog__post__categories-list__item list-inline-item m-0">
+                                    <a href="#" class="content-blog__post__categories__link">
+                                        <?php echo $category->name; ?>
+                                    </a>
+                                </li>
+
+                            <?php } ?>
+
+                        </ul>
+                    </div>
+
+                </div>
+                <div class="content-blog__post__box p-3 text-center">
+                    <h3 class="content-blog__post__title">
+                        <a href="<?php echo get_permalink($post->ID); ?>" class="content-blog__link">
+                            <?= $post->post_title ?>
+                        </a>
+                    </h3>
+                    <h5 class="content-blog__post__subtitle text-center">
+                        Publicado em
+                        <?= the_time('j \d\e F \d\e Y', $post->post_date) ?>
+                    </h5>
+
+                    <h4 class="content-blog__post__description">
+                        <?= $post->post_excerpt ?>
+                    </h4>
+                    <a href="<?php echo get_permalink($post->ID); ?>" class="content-blog__post__button">
+                        Ler mais...
+                    </a>
+                </div>
+
+            </div>
+
+        <?php } ?>
+
+    </div>
+</section>
+
+<section id="section-contact" class="container-full content-main">
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h3 class="title-section bold"></h3>
+                <h3 class="title-section bold">Contato</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="content-contact text-center">
+                    <ul class="content-contact__list list-inline">
+                        <li class="content-contact__list-item list-inline-item">
+                            <a href="" class="content-contact__link">
+                                Icone Telegram
+                            </a>
+                        </li>
+                        <li class="content-contact__list-item list-inline-item">
+                            <a href="" class="content-contact__link">
+                                Icone Whatsapp
+                            </a>
+                        </li>
+                        <li class="content-contact__list-item list-inline-item">
+                            <a href="" class="content-contact__link">
+                                Icone Github
+                            </a>
+                        </li>
+                        <li class="content-contact__list-item list-inline-item">
+                            <a href="" class="content-contact__link">
+                                Icone Email
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
