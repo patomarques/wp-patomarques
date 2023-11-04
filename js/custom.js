70b66d94c992d8ac8d77a5d38368739e8a16f538
Pato Marques <patoxmarques@gmail.com>
2023-11-04 12:03:37 -0300

    menu fixed when scrolled

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,25 @@
 jQuery(function ($) {
 
-    // Do stuff here
+    $('a[href=#target]').
+        click(function () {
+            var target = $('a[name=target]');
+            if (target.length) {
+                var top = target.offset().top;
+                $('html,body').animate({ scrollTop: top }, 2000);
+                return false;
+            }
+        });
+});
 
-}); // jQuery End
+window.addEventListener("load", (event) => {
+    let menu = document.getElementById('masthead');
+    let offset = document.getElementById("section-home-intro").offsetHeight;
+    window.onscroll = function() {
+        if (window.scrollY > offset-10) {
+            menu.classList.add("fixed-top");
+        } else if(window.scrollY < offset-20) {
+            menu.classList.remove("fixed-top");
+        }
+    }
+    console.log("page is fully loaded", offset);
+});
